bbbe33da3e1cbce3774a17335130c439d0259d39
Stijn Janmaat <stijnjanmaat@gmail.com>
2026-01-22 20:13:00 +0100

    use tanstack example versions

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,24 @@
 import { defineConfig } from 'vite'
-import tsConfigPaths from 'vite-tsconfig-paths'
+import viteTsConfigPaths from 'vite-tsconfig-paths'
 import viteReact from '@vitejs/plugin-react'
+import { devtools } from '@tanstack/devtools-vite'
 import { tanstackStart } from '@tanstack/react-start/plugin/vite'
 import tailwindcss from '@tailwindcss/vite'
 import path from 'path'
+import { nitro } from 'nitro/vite';
 
 // TanStack Start configuration
 // Reference: https://tanstack.com/start/latest/docs/framework/react/build-from-scratch
 export default defineConfig({
   plugins: [
-    tsConfigPaths(), // Handles path aliases from tsconfig.json
+    devtools(),
+    viteTsConfigPaths({
+      projects: ['./tsconfig.json'],
+    }),
+    nitro(),
+    tailwindcss(),
     tanstackStart(), // TanStack Start plugin - must come before viteReact
     viteReact(), // React plugin - must come after tanstackStart
-    tailwindcss(),
   ],
   resolve: {
     alias: {
